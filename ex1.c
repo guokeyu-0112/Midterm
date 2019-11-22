58d016fa5c326dbcaba8a71fe50422cb19d189d9
guokeyu-0112 <2897868864@qq.com>
2019-11-22 00:04:48 -0800

    added a ex1.c file

---
+++ ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    int a=0,word=0;
+    printf("please enter a sentence");
+    while((ch=getchar())!='\n')
+    if(ch<'a'||ch>'z')
+    word=0;
+    else if(ch<'A'||ch>'Z')
+    word=0;
+    else if(word==0)
+    {
+        word=1;
+        a++;
+    }
+    printf("the number of words is:%d",a);
+    return 0;
+}
